5028f85ee22bf0157bb736701900cd62b575929e
Antonio <antonio241710@gmail.com>
2020-04-25 17:50:55 +0200

    página de errores

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
-
 import VueRouter from "vue-router";
-
-import HomeComponent from "./components/HomeComponent";
-
-import AboutComponent from "./components/AboutComponent";
+import HomeComponent from "./views/HomeComponent";
+import AboutComponent from "./views/AboutComponent";
+import NotFound from "./views/NotFound";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/home', component: HomeComponent},
-        { path: '/about', component: AboutComponent},
+        { path: '/home', component: HomeComponent },
+        { path: '/about', component: AboutComponent },
+        { path: '*', component: NotFound },
     ]
 });
---
+++ resources/js/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-center text-4x1 font-medium">404 not found</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
